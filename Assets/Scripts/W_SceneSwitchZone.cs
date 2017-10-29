a780f067d8100b3da3f402f294d1e6bf6ba502bc
Brandon Hua <huabrandon0@users.noreply.github.com>
2017-10-28 19:21:21 -0700

    Completed workshop 3 material
    Added a title screen, slightly adjusted the mapping.

---
+++ Assets/Scripts/W_SceneSwitchZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W_SceneSwitchZone : MonoBehaviour {
+
+	public string nextSceneName = "";
+
+	W_SceneSwitcher switcher;
+
+	void Start() {
+		switcher = GetComponent<W_SceneSwitcher>();
+	}
+
+	void OnCollisionEnter() {
+		switcher.SwitchScene(nextSceneName);
+	}
+}
